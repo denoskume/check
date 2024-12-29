25ede9641698e627802ccee04bda60990a2f5f0f
Denos Kume <110854822+denoskume@users.noreply.github.com>
2024-12-29 08:59:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,31 +35,11 @@ document.querySelectorAll('.nav-link').forEach(link => {
             }
         }
 
-// Download Resume as PDF
+
+// Print Resume
 document.getElementById('content').addEventListener('click', function(event) {
-    if (event.target.id === 'download-btn') {
-        const { jsPDF } = window.jspdf;
-        const cvElement = document.getElementById('cv');
-
-        html2canvas(cvElement).then(canvas => {
-            const imgData = canvas.toDataURL('image/png');
-            const pdf = new jsPDF('p', 'mm', 'a4');
-            const imgWidth = 210; // A4 width in mm
-            const pageHeight = 295; // A4 height in mm
-            const imgHeight = canvas.height * imgWidth / canvas.width;
-            let heightLeft = imgHeight;
-            let position = 0;
-
-            pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
-            heightLeft -= pageHeight;
-
-            while (heightLeft >= 0) {
-                position = heightLeft - imgHeight;
-                pdf.addPage();
-                pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
-                heightLeft -= pageHeight;
-            }
-            pdf.save('resume.pdf');
-        });
+    if (event.target.id === 'print-btn') {
+        window.print();
     }
 });
+
