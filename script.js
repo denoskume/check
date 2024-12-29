863cdfffeb9c931f5676b8736c1743ef7409d8c5
Denos Kume <110854822+denoskume@users.noreply.github.com>
2024-12-29 08:02:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -34,4 +34,20 @@ document.querySelectorAll('.nav-link').forEach(link => {
                 seeMore.innerHTML = "...see more";
             }
         }
-    
+
+// Download Resume as PDF
+document.getElementById('download-btn').addEventListener('click', function() {
+    const { jsPDF } = window.jspdf;
+
+    const doc = new jsPDF();
+    const content = document.getElementById('content');
+
+    doc.html(content, {
+        callback: function (doc) {
+            doc.save('resume.pdf');
+        },
+        x: 10,
+        y: 10
+    });
+});
+
