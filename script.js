d8068088609868f8d998532fdbbd9b129d5c33e1
Denos Kume <110854822+denoskume@users.noreply.github.com>
2024-12-29 18:37:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -43,24 +43,4 @@ document.getElementById('content').addEventListener('click', function(event) {
     }
 });
 
-        // JavaScript to handle the pop-up form
-        $(document).ready(function() {
-            var modal = document.getElementById("myModal");
-            var btn = document.getElementById("myBtn");
-            var span = document.getElementsByClassName("close")[0];
-
-            btn.onclick = function() {
-                modal.style.display = "block";
-            }
-
-            span.onclick = function() {
-                modal.style.display = "none";
-            }
-
-            window.onclick = function(event) {
-                if (event.target == modal) {
-                    modal.style.display = "none";
-                }
-            }
-        });
-   
+        
